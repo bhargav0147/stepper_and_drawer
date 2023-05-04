9462dfe3baecb2c393b0ae1ce84703917c30a5b9
Lenovo <bhargavpatel6832@gmail.com>
2023-05-04 18:04:52 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'dart:html';
+
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:stepper_and_drawer/Screen/homepage.dart';
+import 'package:stepper_and_drawer/provider/stepper_provider.dart';
+
+void main() {
+  runApp(DevicePreview(
+    builder: (context) {
+      return ChangeNotifierProvider(
+        create: (context) => StepperProvider(),
+        child: MaterialApp(
+          useInheritedMediaQuery: true,
+          locale: DevicePreview.locale(context),
+          builder: DevicePreview.appBuilder,
+          theme: ThemeData.light(),
+          darkTheme: ThemeData.dark(),
+          debugShowCheckedModeBanner: false,
+          routes: {
+            '/': (context) => HomeScreen()
+          },
+        ),
+      );
+    },
+    enabled: !kReleaseMode,
+  ));
+}
